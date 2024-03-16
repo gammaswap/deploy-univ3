6cfc7a8143d3cef6dd3e85d8c1b08c63386ba2b2
Daniel Alcarraz <dam417@nyu.edu>
2024-03-16 13:08:38 -0400

    adding hardhat contract sizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,8 @@ import { HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox'
 import '@nomicfoundation/hardhat-foundry'
 
+require("hardhat-contract-sizer"); // "npx hardhat size-contracts" or "yarn run hardhat size-contracts"
+
 dotenv.config()
 
 const config: HardhatUserConfig = {
